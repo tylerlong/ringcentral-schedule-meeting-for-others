7b6f1427a1eb0b4867276da273386297dea1a80f
Tyler Long <tyler4long@gmail.com>
2021-03-02 10:09:27 -0800

    RC API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world!');
+import RingCentral from '@rc-ex/core';
+import Rest from '@rc-ex/core/lib/Rest';
+
+const rc = new RingCentral({
+  server: Rest.productionServer,
+  token: {
+    access_token: process.env.RINGCENTRAL_ACCESS_TOKEN,
+  },
+});
+
+(async () => {
+  const ext = await rc.restapi().account().extension().get();
+  console.log(ext);
+})();
